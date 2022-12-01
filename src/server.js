bd7da2d907828eb25dd7133859205e8c3e81f559
Mayranne <mayranne-@hotmail.com>
2022-12-01 14:54:48 +0000

    change server.js file

--- src/server.js
+++ src/server.js
@@ -8,13 +8,14 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 5001;
 
+app.use(express.json());
+app.use(userRouter);
+
 // backend connection
 app.get("/health", (req, res) => {
   res.status(200).send({ message: "API is working" });
 }); // backend connection
 
-app.use(express.json());
-app.use(userRouter);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
